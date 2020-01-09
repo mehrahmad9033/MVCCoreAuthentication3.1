6002d5509e2d33b1fd8821d57268d31525685e06
Ahmad Mukhtar <mehrahmad9033@gmail.com>
2020-01-09 15:20:49 +0400

    First Commit

---
+++ MVCAUTHENTICATION/Controllers/LoginController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MVCAUTHENTICATION.Models;
+
+namespace MVCAUTHENTICATION.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IActionResult Login(string username, string password)
+        {
+            LoginModel login = new LoginModel();
+            login.Email = username;
+            login.Password = password;
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenStr = GenerateJsonWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+            return response;
+        }
+
+        private object GenerateJsonWebToken(LoginModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credientials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credientials
+                );
+            var encodedtoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodedtoken;
+        }
+
+        private LoginModel AuthenticateUser(LoginModel login)
+        {
+            LoginModel user = null;
+            if (login.Email == "admin" && login.Password == "admin")
+            {
+                user = new LoginModel()
+                {
+                    Email = "mehrahmad9033@gmail.com"
+                };
+            }
+            return user;
+        }
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            var userName = claim[0].Value;
+            return "Welcome to: " + userName;
+        }
+        [HttpGet("GetValue")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Value1", "Value2", "Value3" };
+        }
+    }
+}
---
+++ MVCAUTHENTICATION/Models/LoginModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAUTHENTICATION.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
